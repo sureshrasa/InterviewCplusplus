f137201a079cbe38547da43bcd1f3bde0d74ca4e
Suresh Rasakulasuriar <suresh@suriar.me.uk>
2018-09-19 12:27:07 +0100

    Merged Trie elements for simplicity

--- Trie.hpp
+++ Trie.hpp
@@ -34,83 +34,51 @@ namespace suriar
     template<typename K, typename V>
     class HashTrie: public Trie<K, V>
     {
-    private:
-        class TrieElement: public Interface<TrieElement>
-        {
-        public:
-            typedef typename K::const_iterator key_iterator;
-            typedef boost::iterator_range<key_iterator> iterator_range;
-            typedef std::pair<iterator_range, boost::optional<V>> result_type;
-
-            typedef std::shared_ptr<TrieElement const> const_pointer_type;
-            typedef std::shared_ptr<TrieElement> pointer_type;
-
-            virtual boost::optional<V> const getValue() const = 0;
-            virtual result_type longest_prefix(K const & key) = 0;
-
-            virtual void put(iterator_range const & range, V const & value) = 0;
-        };
-
-        class NodeTrieElement:
-                public TrieElement,
-                public std::enable_shared_from_this<TrieElement>
-        {
-        public:
-            boost::optional<V> const getValue() const override { return value; }
-
-            typename TrieElement::result_type longest_prefix(K const & key) override
-            {
-                auto const begin = std::begin(key);
-                auto const end = std::end(key);
-                auto trie = std::enable_shared_from_this<TrieElement>::shared_from_this();
-                for (auto i = begin; i != end; ++i)
-                {
-                    auto const pos = children.find(*i);
-                    if (pos == children.end())
-                    {
-                        return std::make_pair(boost::make_iterator_range(begin, i), trie->getValue());
-                    }
-                    trie = pos->second;
-                }
-                return std::make_pair(boost::make_iterator_range(begin, end), trie->getValue());
-            }
-
-            void put(typename TrieElement::iterator_range const & range, V const & value) override
-            {
-                auto const curr = std::begin(range);
-                if (curr == std::end(range))
-                {
-                    this->value = value;
-                }
-                else
-                {
-                    auto & child = children[*curr];
-                    if (!child)
-                    {
-                        child = std::make_shared<NodeTrieElement>();
-                    }
-                    child->put(boost::make_iterator_range(std::next(curr), std::end(range)), value);
-                }
-            }
-
-        private:
-            boost::optional<V> value;
-            std::map<typename K::value_type, typename TrieElement::pointer_type> children;
-        };
-
-        typename TrieElement::pointer_type const root;
     public:
-        HashTrie(): root{std::make_shared<NodeTrieElement>()} {}
-
-        typename Trie<K,V>::result_type longest_prefix(K const & key) const override
+        typename Trie<K, V>::result_type longest_prefix(K const & key) const override
         {
-            return root->longest_prefix(key);
+            auto const begin = std::begin(key);
+            auto const end = std::end(key);
+            auto * trie = this;
+
+            for (auto i = begin; i != end; ++i)
+            {
+                auto const pos = trie->children.find(*i);
+                if (pos == trie->children.end())
+                {
+                    return std::make_pair(boost::make_iterator_range(begin, i), trie->value);
+                }
+                trie = pos->second.get();
+            }
+            return std::make_pair(boost::make_iterator_range(begin, end), trie->value);
         }
 
         void put(K const & key, V const & value) override
         {
-            root->put(boost::make_iterator_range(std::begin(key), std::end(key)), value);
+            put(boost::make_iterator_range(std::begin(key), std::end(key)), value);
         }
+
+    private:
+        void put(typename Trie<K, V>::iterator_range const & range, V const & value)
+        {
+            auto const curr = std::begin(range);
+            if (curr == std::end(range))
+            {
+                this->value = value;
+            }
+            else
+            {
+                auto & child = children[*curr];
+                if (!child)
+                {
+                    child = std::make_shared<HashTrie>();
+                }
+                child->put(boost::make_iterator_range(std::next(curr), std::end(range)), value);
+            }
+        }
+
+        boost::optional<V> value;
+        std::map<typename K::value_type, std::shared_ptr<HashTrie>> children;
     };
 }
 
